47d53ad6cb20a7bc60a7c34dc7a21923808152a8
Jim Fitzgerald <dumples5@gmail.com>
2012-04-19 15:21:15 -0400

    why wont my functions work?

--- website.lisp
+++ website.lisp
@@ -9,18 +9,26 @@
 
 (defun outpatient-insert (uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon diag painlvl)
   "Insert statement for outpatient table"
-   ((execute-non-query db "insert into outpatient values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-                      uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon diag painlvl)))
+   ((let (db (connect "mvch")))
+    (execute-non-query db "insert into outpatient values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                      uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon diag painlvl)
+    (disconnect db))
 
 (defun resident-insert (uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon date-admit date-disch bed-id painlvl diag work-unit)
   "Insert statement for resident table"
-   (execute-non-query db "insert into resident values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-                       uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon date-admit date-disch bed-id painlvl diag work-unit))
+   ((let (db (connect "mvch")))
+    (execute-non-query db "insert into resident values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                       uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon date-admit date-disch bed-id painlvl diag work-unit)
+    (disconnect db))
 
 (defun outpatient-select-all ()
   "Selects all items from outpatient table and puts them into a list"
-   (execute-to-list db "select * from outpatient"))
+   ((let (db (connect "mvch")))
+   (execute-to-list db "select * from outpatient")
+   (disconnect db))
 
 (defun resident-select-all ()
   "Selects all items from resident table and puts them into a list"
-  (execute-to-list db "select * from resident"))
+  ((let (db (connect "mvch")))
+   (execute-to-list db "select * from resident")
+   (disconnect db))
