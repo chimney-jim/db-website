3039ca07f675506e82f48daf133dc1572b8e16ca
Jim Fitzgerald <dumples5@gmail.com>
2012-04-19 21:02:27 -0400

    all db functions working

--- website.lisp
+++ website.lisp
@@ -5,30 +5,22 @@
 
 (in-package :db-website)
 
-;;(defvar db (connect "mvch"))
+(defvar db (connect "mvch"))
 
 (defun outpatient-insert (uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon diag painlvl)
   "Insert statement for outpatient table"
-   ((let (db (connect "mvch")))
     (execute-non-query db "insert into outpatient values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-                      uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon diag painlvl)
-    (disconnect db))
+                      uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon diag painlvl))
 
 (defun resident-insert (uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon date-admit date-disch bed-id painlvl diag work-unit)
   "Insert statement for resident table"
-   ((let (db (connect "mvch")))
     (execute-non-query db "insert into resident values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-                       uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon date-admit date-disch bed-id painlvl diag work-unit)
-    (disconnect db))
+                       uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon date-admit date-disch bed-id painlvl diag work-unit))
 
 (defun outpatient-select-all ()
   "Selects all items from outpatient table and puts them into a list"
-   ((let (db (connect "mvch")))
-   (execute-to-list db "select * from outpatient")
-   (disconnect db))
+   (execute-to-list db "select * from outpatient"))
 
 (defun resident-select-all ()
   "Selects all items from resident table and puts them into a list"
-  ((let (db (connect "mvch")))
-   (execute-to-list db "select * from resident")
-   (disconnect db))
+  (execute-to-list db "select * from resident"))
