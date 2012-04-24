54a342abff4fbeb073155479987e246164e4ce18
Jim Fitzgerald <dumples5@gmail.com>
2012-04-24 14:41:15 -0400

    broke something at character 422

--- html.lisp
+++ html.lisp
@@ -9,31 +9,30 @@
   `(with-html-output-to-string
      (*standard-output* nil :prologue t),@body))
 
-(define-easy-handler (say-yo :uri "/yo") (name)
+(hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
                      (setf (content-type*) "text/plain")
                      (format nil "Hey~@[ ~A~]!" name))
 
-;(define-easy-handler (patients :uri "/patients") ((uid :parameter-type 'integer)
-;                                                  fname lname addr city state
-;                                                  (zip :parameter-type 'integer)
-;                                                  (dob :parameter-type 'integer)
-;                                                  (phone :parameter-type 'integer)
-;                                                  email 
-;                                                  (emrcon :parameter-type 'integer)
-;                                                  insurinfo
-;                                                  (insurcon :parameter-type 'integer)
-;                                                  diag 
-;                                                  (painlvl :parameter-type 'integer))
-;                     (with-html
-;                       (:html
-;                         (:head (:title "Patient Database Interaction"))
-;                         (:body
-;                           (:h1 "Hello World!!")))))
-
-
-;(defun main-page ()
-;    (with-html
-;      (:html
-;        (:head (:title "Hello World"))
-;        (:body
-;          (:p (:form :method
+(hunchentoot:define-easy-handler (patients :uri "/patients" :default-request-type :post )
+                                 ((uid :parameter-type 'integer)
+                                    fname lname addr city state
+                                    (zip :parameter-type 'integer)
+                                    (dob :parameter-type 'integer)
+                                    (phone :parameter-type 'integer)
+                                    email 
+                                    (emrcon :parameter-type 'integer)
+                                    insurinfo
+                                    (insurcon :parameter-type 'integer)
+                                    diag 
+                                    (painlvl :parameter-type 'integer))
+                     (with-html
+                       (:html
+                         (:head (:title "Patient Database Interaction"))
+                         (:body
+                           (:p (:form :method :post
+                                      (:table :border 1 :cellpadding 2 :cellspacing 0
+                                              (:tr
+                                                (:td "First Name:")
+                                                (:td (:input :type :text
+                                                     :name "fname"
+                                                     :value)))))))))
