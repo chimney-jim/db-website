d85242bd64771a9b63c33782079e9c78c58f19fe
Jim Fitzgerald <dumples5@gmail.com>
2012-04-24 15:06:09 -0400

    added more input boxes

--- html.lisp
+++ html.lisp
@@ -32,7 +32,42 @@
                            (:p (:form :method :post
                                       (:table :border 1 :cellpadding 2 :cellspacing 0
                                               (:tr
+                                                (:td "UID:")
+                                                (:td (:input :type :text
+                                                             :name "uid" :value)))
+                                              (:tr
                                                 (:td "First Name:")
                                                 (:td (:input :type :text
-                                                     :name "fname"
-                                                     :value))))))))))
+                                                             :name "fname" :value)))
+                                              (:tr
+                                                (:td "Last Name:")
+                                                (:td (:input :type :text
+                                                             :name "lname" :value)))
+                                              (:tr
+                                                (:td "Address: ")
+                                                (:td (:input :type :text
+                                                             :name "addr" :value)))
+                                              (:tr
+                                                (:td "City: ")
+                                                (:td (:input :type :text
+                                                             :name "city" :value)))
+                                              (:tr
+                                                (:td "State: ")
+                                                (:td (:input :type :text
+                                                             :name "state" :value)))
+                                              (:tr
+                                                (:td "Zip: ")
+                                                (:td (:input :type :text
+                                                             :name "zip" :value)))
+                                              (:tr
+                                                (:td "Date of Birth: ")
+                                                (:td (:input :type :text
+                                                             :name "dob" :value)))
+                                              (:tr
+                                                (:td "Phone Number: ")
+                                                (:td (:input :type :text
+                                                             :name "phone" :value)))
+                                              (:tr
+                                                (:td "Email: ")
+                                                (:td (:input :type :text
+                                                             :name "email" :value))))))))))
