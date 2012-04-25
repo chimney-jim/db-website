859f0170d02d6cba55324ec90e0a19a3801c7a81
Jim Fitzgerald <dumples5@gmail.com>
2012-04-25 00:18:48 -0400

    moved all main files into src folder

---
+++ src/html.lisp
@@ -0,0 +1,95 @@
+(ql:quickload '(cl-who hunchentoot parenscript sqlite))
+
+(defpackage :html
+  (:use :cl :cl-who :hunchentoot :parenscript :asdf))
+
+(in-package :html)
+
+(defmacro with-html (&body body)
+  `(with-html-output-to-string
+     (*standard-output* nil :prologue t),@body))
+
+(hunchentoot:define-easy-handler (patients :uri "/patients" :default-request-type :post )
+                                 ((uid :parameter-type 'integer)
+                                    fname lname addr city state
+                                    (zip :parameter-type 'integer)
+                                    (dob :parameter-type 'integer)
+                                    (phone :parameter-type 'integer)
+                                    email 
+                                    (emrcon :parameter-type 'integer)
+                                    insurinfo
+                                    (insurcon :parameter-type 'integer)
+                                    diag 
+                                    (painlvl :parameter-type 'integer))
+                     (with-html
+                       (:html
+                         (:head (:title "Patient Database Interaction"))
+                         (:body
+                           (:p (:form :method :post
+                                      (:table :border 1 :cellpadding 2 :cellspacing 0
+                                              (:tr
+                                                (:td "UID:")
+                                                (:td (:input :type :text
+                                                             :name "uid" :value)))
+                                              (:tr
+                                                (:td "First Name:")
+                                                (:td (:input :type :text
+                                                             :name "fname" :value)))
+                                              (:tr
+                                                (:td "Last Name:")
+                                                (:td (:input :type :text
+                                                             :name "lname" :value)))
+                                              (:tr
+                                                (:td "Address: ")
+                                                (:td (:input :type :text
+                                                             :name "addr" :value)))
+                                              (:tr
+                                                (:td "City: ")
+                                                (:td (:input :type :text
+                                                             :name "city" :value)))
+                                              (:tr
+                                                (:td "State: ")
+                                                (:td (:input :type :text
+                                                             :name "state" :value)))
+                                              (:tr
+                                                (:td "Zip: ")
+                                                (:td (:input :type :text
+                                                             :name "zip" :value)))
+                                              (:tr
+                                                (:td "Date of Birth: ")
+                                                (:td (:input :type :text
+                                                             :name "dob" :value)))
+                                              (:tr
+                                                (:td "Phone Number: ")
+                                                (:td (:input :type :text
+                                                             :name "phone" :value)))
+                                              (:tr
+                                                (:td "Email: ")
+                                                (:td (:input :type :text
+                                                             :name "email" :value)))
+                                              (:tr
+                                                (:td "Emergency Contact: ")
+                                                (:td (:input :type :text
+                                                             :name "emercon" :value)))
+                                              (:tr
+                                                (:td "Insurance Info: ")
+                                                (:td (:input :type :text
+                                                             :name "insurinfo" :value)))
+                                              (:tr
+                                                (:td "Insurance Contact: ")
+                                                (:td (:input :type :text
+                                                             :name "insurcon" :value)))
+                                              (:tr
+                                                (:td "Diagnosis: ")
+                                                (:td (:input :type :text
+                                                             :name "diag" :value)))
+                                              (:tr
+                                                (:td "Pain Level: ")
+                                                (:td (:input :type :text
+                                                             :name "painlvl" :value)))
+                                              (:tr
+                                                (:td :colspan 2
+                                                     (:input :type "submit"))))))
+                     (sql-util:outpatient-insert (uid fname lname addr city state zip dob 
+                                                    phone email emercon insurinfo insurcon
+                                                    diag painlvl))))))
---
+++ src/sql-util.lisp
@@ -0,0 +1,32 @@
+(defpackage :sql-util
+  (:nicknames :sql-util)
+  (:use :cl :sqlite)
+  (:export :outpatient-insert
+           :resident-insert
+           :outpatient-select-all
+           :resident-select-all))
+
+(in-package :sql-util)
+
+(defvar db (connect "mvch"))
+
+(defun outpatient-insert (uid fname lname addr city state zip dob
+                            phone email emrgcon insurinfo insurcon diag painlvl)
+  "Insert statement for outpatient table"
+    (execute-non-query db "insert into outpatient values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                      uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon diag painlvl))
+
+(defun resident-insert (uid fname lname addr city state zip dob
+                        phone email emrgcon insurinfo insurcon
+                        date-admit date-disch bed-id painlvl diag work-unit)
+  "Insert statement for resident table"
+    (execute-non-query db "insert into resident values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                       uid fname lname addr city state zip dob phone email emrgcon insurinfo insurcon date-admit date-disch bed-id painlvl diag work-unit))
+
+(defun outpatient-select-all ()
+  "Selects all items from outpatient table and puts them into a list"
+   (execute-to-list db "select * from outpatient"))
+
+(defun resident-select-all ()
+  "Selects all items from resident table and puts them into a list"
+  (execute-to-list db "select * from resident"))
